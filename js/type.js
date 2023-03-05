fa0e0afc2c32c4b33268a40cca87c8f513abb372
weez rodriguez <w0812903@myscc.ca>
2023-03-05 15:14:34 -0500

    Initial commit

---
+++ js/type.js
@@ -0,0 +1,8 @@
+/* typing anime */
+var typed = new Typed(".typing",{
+	strings:["","Veteran - Air Force","Front-End Developer","Web Designer","Web Developer","Graphic Designer","Father", "UI/UX Developer"],
+	typeSpeed:100,
+	BackSpeed:60,
+	loop: true
+	
+})
